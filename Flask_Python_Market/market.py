86aeb45006771e0112b45baad228b00a49fedc2d
JuanmiAcosta <acostaojuanmi@gmail.com>
2024-02-15 13:05:32 +0100

    avanzando en la página de market

--- Flask_Python_Market/market.py
+++ Flask_Python_Market/market.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # export FLASK_APP=market.py
 # export FLASK_DEBUG=1
@@ -7,10 +7,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
 
-@app.route('/about/<username>') #dynamic route
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    items = [ #lista de diccionarios
+        {'id': 1, 'name': 'Meta Quest 3', 'barcode': '893212299897', 'price': 400},
+        {'id': 2, 'name': 'Asus Tuf Gaming A15 2023', 'barcode': '123985473165', 'price': 1300},
+        {'id': 3, 'name': 'Samsung Tab S7', 'barcode': '231985128446', 'price': 700},
+    ]
+    return render_template('market.html', items = items)
+
+
+@app.route('/about/<username>')  # dynamic route
 def about_page(username):
     return f'<h1>This is the about page of {username}</h1>'
